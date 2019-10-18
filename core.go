fc66581f6fa9cfcde823e1ee81e008246342f75d
tenm <tenguyen@vietnambiz.com>
2019-10-18 10:26:28 +0700

    add pumpin pumpout and make server

--- core.go
+++ core.go
@@ -1,63 +1,62 @@
 package main
 
-import(
+import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
-	"log"
+
 	"github.com/gorilla/websocket"
 )
+
+var MAX_SIZE = 1024
+var PONG_WAIT = 2 * time.Minute
+var WRITE_WAIT = 10 * time.Second
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-
 type Message struct {
 	payload []byte
 	created int64
-	Type string
+	Type    string
 	eventId string
 }
 
 type Command struct {
-	client *Client
-	Type string
+	client  *Client
+	Type    string
 	payload string
 }
 
 type Ws struct {
-	clients map[*Client] bool
+	clients   map[*Client]bool
 	broadcast chan *Message
-	register chan *Command
+	register  chan *Command
 	mapEvents map[string][]*Client
 }
 
 type Client struct {
-	id string
-	ws *Ws
-	conn *websocket.Conn
+	id     string
+	ws     *Ws
+	conn   *websocket.Conn
 	sender chan []byte
 }
 
-func startServe() {
-	ws := initWs()
-}
-
-
 func initWs() *Ws {
 	return &Ws{
-		broadcast:  make(chan *Message),
-		register:   make(chan *Command),
-		clients:    make(map[*Client]bool),
-		mapEvents:  make(map[string][]*Client),
+		broadcast: make(chan *Message),
+		register:  make(chan *Command),
+		clients:   make(map[*Client]bool),
+		mapEvents: make(map[string][]*Client),
 	}
 }
 
-
-func(ws *Ws) start() {
+func (ws *Ws) start() {
 	for {
 		select {
-		case cmd := <- ws.register:
+		case cmd := <-ws.register:
 			switch cmd.Type {
 			case "connected":
 				ws.clients[cmd.client] = true
@@ -81,7 +80,7 @@ func(ws *Ws) start() {
 				}
 				removeItemFromSlice(ws.mapEvents[cmd.payload], cmd.client)
 			}
-		case message := <- ws.broadcast:
+		case message := <-ws.broadcast:
 			// Client subEvent
 			for _, client := range ws.mapEvents[message.eventId] {
 				client.sender <- []byte(message.payload)
@@ -90,40 +89,40 @@ func(ws *Ws) start() {
 	}
 }
 
-func(ws*Ws) closeConnection(client *Client) {
+func (ws *Ws) closeConnection(client *Client) {
 	delete(ws.clients, client)
 	close(client.sender)
 }
 
-func(client *Client)initClient(ws *Ws, w http.ResponseWriter, r *http.Request) {
+func initClient(ws *Ws, w http.ResponseWriter, r *http.Request) *Client {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		panic(err)
 	}
-	client = &Client{
-		id: makeID(13),
-		conn: conn,
+	client := &Client{
+		id:     makeID(17),
+		conn:   conn,
+		ws:     ws,
 		sender: make(chan []byte, 1024),
 	}
-	ws.register <- &Command {
-		Type: "connected",
+	ws.register <- &Command{
+		Type:   "connected",
+		client: client,
 	}
+	return client
 }
 
-
 // READ DATA FROM CLIENT SENT
-func(client *Client) inPump() {
-	MAX_SIZE := 1024
-	PONG_WAIT := 2 * time.Minute
+func (client *Client) inPump() {
 	defer func() {
-		client.ws.register <- &Command { Type: "disconnected" }
+		client.ws.register <- &Command{Type: "disconnected", client: client}
 		client.conn.Close()
 		close(client.sender)
 	}()
 	client.conn.SetReadLimit(int64(MAX_SIZE))
 	client.conn.SetReadDeadline(time.Now().Add(PONG_WAIT))
 	client.conn.SetPongHandler(func(string) error {
-		 client.conn.SetReadDeadline(time.Now().Add(PONG_WAIT))
+		client.conn.SetReadDeadline(time.Now().Add(PONG_WAIT))
 		return nil
 	})
 	for {
@@ -135,31 +134,70 @@ func(client *Client) inPump() {
 			break
 		}
 		// DO Somthing
-		log.Print(message)
-		// client.ws.broadcast <- &Message {
-		// 	created: time.Now().UnixNano(),
-		// 	payload: message,
-		// 	eventId:
-		// }
+		log.Print("message received: ", string(message))
+		cmd := &Command{}
+		if err := json.Unmarshal(message, cmd); err != nil {
+			log.Fatal(err)
+		}
+		// just using for command
+		cmd.client = client
+		client.ws.register <- cmd
 	}
 }
 
-func(client *Client) outPump() {
+func (client *Client) outPump() {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer func() {
+		ticker.Stop()
+		client.ws.register <- &Command{Type: "disconnected", client: client}
+		client.conn.Close()
+		close(client.sender)
+	}()
+	for {
+		select {
+		case message, ok := <-client.sender:
+			client.conn.SetWriteDeadline(time.Now().Add(WRITE_WAIT))
+			if !ok {
+				// The hub closed the channel.
+				client.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
 
+			w, err := client.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			for i := 0; i < len(message); i++ {
+				w.Write(message)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			client.conn.SetWriteDeadline(time.Now().Add(WRITE_WAIT))
+			if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
 }
 
-func(client *Client) subscribe(eventID string) {
+func (client *Client) subscribe(eventID string) {
 	client.ws.register <- &Command{
-		client: client,
-		Type: "subscribed",
+		client:  client,
+		Type:    "subscribed",
 		payload: eventID,
 	}
 }
 
-func(client *Client) unsubscribe(eventID string) {
+func (client *Client) unsubscribe(eventID string) {
 	client.ws.register <- &Command{
-		client: client,
-		Type: "unsubscribed",
+		client:  client,
+		Type:    "unsubscribed",
 		payload: eventID,
 	}
 }
--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"net/http"
 )
 
+var addr = flag.String("addr", ":8000", "http service address")
+
 func main() {
-	fmt.Print()
+	flag.Parse()
+	ws := initWs()
+	go ws.start()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		client := initClient(ws, w, r)
+		go client.inPump()
+		go client.outPump()
+	})
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
